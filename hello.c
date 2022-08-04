facd4af72ef42356ce04887de6ce0858ce7eb780
huiyue19111 <1161391384@qq.com>
2022-08-04 11:54:53 +0800

    hello-1

--- hello.c
+++ hello.c
@@ -3,15 +3,3 @@ int main(int argc, const char *argv[])
 {
 	printf("hello world\n");	
 	printf("hello world\n");	
-	printf("hello world\n");	
-	printf("hello world\n");	
-	printf("hello world\n");	
-	printf("hello world\n");	
-	printf("hello world\n");	
-	printf("hello world\n");	
-	printf("hello world\n");	
-	printf("hello world\n");	
-	printf("hello world\n");	
-
-	return 0;
-}
